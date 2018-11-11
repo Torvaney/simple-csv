e5e4149c28ee25498be82d92efe0ddb38941835e
Torvaney <torvaney@protonmail.com>
2018-11-11 00:53:10 +0000

    Enable column addition/deletion

--- src/Main.elm
+++ src/Main.elm
@@ -26,22 +26,32 @@ main =
 
 init : () -> ( Model, Cmd msg )
 init _ =
-    ( { data = Data.mtcars }
+    ( { data = Data.mtcars
+      , mode = Edit
+      }
     , Cmd.none
     )
 
 
 type alias Model =
     { data : Csv.Csv
+    , mode : Mode
     }
 
 
+type Mode
+    = Edit
+    | Delete
+
+
 type Msg
-    = AddColumn
-    | EditCell CellInfo
+    = AddColumn String
+    | RemoveColumn Int
+    | EditCell Cell
+    | ChangeMode Mode
 
 
-type alias CellInfo =
+type alias Cell =
     { value : String
     , location : CellLocation
     }
@@ -55,17 +65,36 @@ type CellLocation
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        AddColumn ->
-            ( model, Cmd.none )
+        AddColumn name ->
+            ( { model
+              | data = addColumn model.data name
+              }
+            , Cmd.none
+            )
 
         EditCell new ->
-            Debug.log new.value
             ( updateCell new model, Cmd.none )
+
+        ChangeMode mode ->
+            ( { model
+              | mode = mode
+              }
+            , Cmd.none
+            )
+
+        RemoveColumn ix ->
+            ( { model
+              | data = removeColumn model.data ix
+              }
+            , Cmd.none
+            )
+
 
 
 -- TODO: Test the spreadhseet updating logic
 -- Also, consider submitting a PR to lovasoa/elm-csv?
 
+updateCell : Cell -> Model -> Model
 updateCell cell model =
     case cell.location of
         Header col ->
@@ -91,6 +120,20 @@ setBody data newRecords =
     { data | records = newRecords }
 
 
+addColumn : Csv.Csv -> String -> Csv.Csv
+addColumn data name =
+    { headers = data.headers ++ [ name ]
+    , records = List.map (\x -> x ++ [ "" ]) data.records
+    }
+
+
+removeColumn : Csv.Csv -> Int -> Csv.Csv
+removeColumn data ix =
+    { headers = removeFromList ix data.headers
+    , records = List.map (removeFromList ix) data.records
+    }
+
+
 changeAt : Int -> a -> List a -> List a
 changeAt ix val list =
     List.indexedMap
@@ -105,7 +148,13 @@ applyAt ix f list =
         list
 
 
+removeFromList : Int -> List a -> List a
+removeFromList i xs =
+  (List.take i xs) ++ (List.drop (i+1) xs)
+
+
 -- View
+-- TODO: Factorise styles
 
 
 view model =
@@ -113,29 +162,89 @@ view model =
         El.column
             [ El.centerX ] -- [ El.centerX, El.spacing 10 ]
             [ El.el
-                [ El.centerX, El.padding 10 ]
+                [ El.centerX
+                , El.padding 10
+                , Font.size 24
+                , Font.bold
+                ]
                 (El.text "Simple Csv Editor")
+            , El.row
+                [ El.alignRight, El.padding 10, El.spacing 5 ]
+                [ deleteButton model.mode, addColumnButton ]
             , viewSpreadsheet model
             ]
+
+
+addColumnButton =
+    Input.button
+        [ Background.color (El.rgb255 240 240 240)
+        , Border.color (El.rgb255 220 220 220)
+        , Border.rounded 20
+        , Border.width 1
+        , El.padding 10
+        , Font.size 16
+        ]
+        { onPress = Just (AddColumn "FIXME")
+        , label   = El.text "Add column"
+        }
+
+
+deleteButton mode =
+    case mode of
+        Edit ->
+            enterDeleteMode
+
+        Delete ->
+            cancelDeleteMode
+
+
+enterDeleteMode =
+    Input.button
+        [ Background.color (El.rgb255 240 200 200)
+        , Border.color (El.rgb255 200 50 50)
+        , Border.rounded 20
+        , Border.width 1
+        , El.padding 10
+        , Font.size 16
+        , Font.color (El.rgb255 150 50 50)
+        ]
+        { onPress = Just (ChangeMode Delete)
+        , label   = El.text "Delete"
+        }
+
+
+cancelDeleteMode =
+    Input.button
+        [ Background.color (El.rgb255 150 50 50)
+        , Border.color (El.rgb255 150 50 50)
+        , Border.rounded 20
+        , Border.width 1
+        , El.padding 10
+        , Font.size 16
+        , Font.color (El.rgb255 240 200 200)
+        ]
+        { onPress = Just (ChangeMode Edit)
+        , label   = El.text "Cancel"
+        }
 
 
 viewSpreadsheet model =
     El.column []
         [ El.row
             [ El.centerX, El.spacing 15 ]
-            [ viewHeader model.data.headers ]
+            [ viewHeader model.mode model.data.headers ]
         , El.row
             [ El.centerX ]
             [ viewCells model.data.records ]
         ]
 
 
-viewHeader headers =
+viewHeader mode headers =
     El.row
         [ El.padding 0
         , El.spacing 0
         ] <|
-        List.indexedMap viewHeaderCell headers
+        List.indexedMap (viewHeaderCell mode) headers
 
 
 viewCells records =
@@ -153,12 +262,21 @@ viewRow ix record =
         List.indexedMap (viewBodyCell ix) record
 
 
-viewHeaderCell colIx value =
-    viewCell
-        (El.rgb255 240 240 240)
-        { value = value
-        , location = Header colIx
-        }
+viewHeaderCell mode colIx value =
+    case mode of
+        Edit ->
+            viewCell
+                (El.rgb255 240 240 240)
+                { value = value
+                , location = Header colIx
+                }
+
+        Delete ->
+            viewCellButton
+                (RemoveColumn colIx)
+                { value = value
+                , location = Header colIx
+                }
 
 
 viewBodyCell colIx rowIx value =
@@ -188,6 +306,26 @@ viewCell fill cell =
         , text        = cell.value
         , placeholder = Nothing
         , label       = Input.labelHidden cell.value
+        }
+
+
+viewCellButton msg cell =
+    Input.button
+        [ El.padding 10
+        , El.spacing 0
+        , El.width <| El.px 80
+        , El.height<| El.px 30
+        , Background.color (El.rgb255 150 50 50)
+        , Border.rounded 2
+        , Border.width 1
+        , Font.size 12
+        , Font.family
+            [ Font.typeface "Monaco"
+            , Font.monospace
+            ]
+        ]
+        { onPress = Just msg
+        , label   = El.text cell.value
         }
 
 
